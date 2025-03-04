ba92af7a240dce15f316cedc35d8abee97300dd9
Diola Chryselle Dsouza <diocrisel9@gmail.com>
2025-03-04 09:48:01 -0800

    Adding previously ignored folder

---
+++ proj4/kernel/show_vm_areas.d
@@ -0,0 +1,8 @@
+proj4/kernel/show_vm_areas.o: proj4/kernel/show_vm_areas.c \
+ /usr/riscv64-linux-gnu/include/stdc-predef.h \
+ proj4/kernel/../../kernel/types.h proj4/kernel/../../kernel/param.h \
+ proj4/kernel/../../kernel/memlayout.h proj4/kernel/../../kernel/riscv.h \
+ proj4/kernel/../../kernel/defs.h proj4/kernel/../../kernel/spinlock.h \
+ proj4/kernel/../../kernel/proc.h proj4/kernel/../../kernel/sleeplock.h \
+ proj4/kernel/../../kernel/fs.h proj4/kernel/../../kernel/file.h \
+ proj4/kernel/../../kernel/fcntl.h
---
+++ proj4/user/vm_areas_test.d
@@ -0,0 +1,3 @@
+proj4/user/vm_areas_test.o: proj4/user/vm_areas_test.c \
+ /usr/riscv64-linux-gnu/include/stdc-predef.h kernel/types.h \
+ kernel/stat.h user/user.h
